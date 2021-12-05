cb5315a92d8aab47605b4b38b1091da9137f0867
Joe Scott <jbs198585@gmail.com>
2021-12-05 06:38:25 -0500

    Fixed href errors

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ class NavBar extends Component {
             >
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                  <a className="nav-link" href="#">
+                  <a className="nav-link" href="/#">
                     Home
                   </a>
                 </li>
